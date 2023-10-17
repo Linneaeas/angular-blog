fc8bf226820cc74f475c75d1b66896395d767b9f
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-10-17 17:46:42 +0200

    Toggle View on Home Page, adding a new post button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CreatorViewComponent } from './Pages/HomePage/CreatorView/creator-view/creator-view.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-blog';
+  showElement: boolean = false;
+  
+  toggleElement() {
+    this.showElement = !this.showElement;
+  }
 }
