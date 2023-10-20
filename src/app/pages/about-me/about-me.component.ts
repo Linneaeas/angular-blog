d94444e2cd65ae70e59491fc6e920f23850c6186
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-10-20 19:05:50 +0200

    Using ngModule: FormsModule to retrive and display the input fields value in the console

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -6,7 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent {
-
 showFormFields: boolean = false;
+firstName: string = '';
+lastName: string = '';
+email: string = '';
+message: string = '';
 
+onSubmit() {
+  console.log('First Name:', this.firstName);
+  console.log('Last Name:', this.lastName);
+  console.log('Email:', this.email);
+  console.log('Message:', this.message);
+
+  this.firstName = '';
+  this.lastName = '';
+  this.email = '';
+  this.message = '';
+  this.showFormFields=false;
+}
 }
