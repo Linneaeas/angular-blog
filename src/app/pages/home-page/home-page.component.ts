ec97314c5fa06bdbeda56e0e0b792c8dcfe7edb3
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-10-20 12:07:45 +0200

    Tried ChangeDetectorRef to make the view content change withouth updating the page, but it did'nt work.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit} from '@angular/core';
 import { ViewStateService, ViewType } from 'src/app/view-state.service';
 
 
@@ -7,21 +7,27 @@ import { ViewStateService, ViewType } from 'src/app/view-state.service';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent  {
+export class HomePageComponent implements OnInit {
   currentView: ViewType;
   Creator = ViewType.Creator; 
   User = ViewType.User;  
 
-  constructor(private viewStateService: ViewStateService) {
+  constructor(private viewStateService: ViewStateService, private cd: ChangeDetectorRef) {
     this.currentView = this.viewStateService.getCurrentView();
   }
   
   toggleView() {
     this.currentView = this.currentView === ViewType.Creator ? ViewType.User : ViewType.Creator;
     this.viewStateService.setCurrentView(this.currentView);
+    this.cd.detectChanges(); 
   }
 
-  
+  ngOnInit() {
+    this.viewStateService.setCurrentView(this.viewStateService.getCurrentView());
+    this.currentView = this.viewStateService.getCurrentView();
+
+  }
+
   get viewType() {
     return this.viewStateService.getCurrentView();
   }
