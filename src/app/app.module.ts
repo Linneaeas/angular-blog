f3085767a72eae40070d55f5ec166a76da67da1f
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-10-20 19:39:54 +0200

    Set up Local storage service and apply it in the "Create new post"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { LocalStorageService } from './local-storage.service';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
@@ -21,7 +22,7 @@ import { CreatePostComponent } from './pages/create-post/create-post.component';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [LocalStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/create-post/create-post.component.ts
+++ src/app/pages/create-post/create-post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from 'src/app/local-storage.service';
 
 @Component({
   selector: 'app-create-post',
@@ -10,6 +11,13 @@ postTitle: string = '';
 postBody: string = '';
 postTags: string = '';
 postPic: File | null = null; 
+
+constructor(private localStorageService: LocalStorageService) {}
+
+savePostToLocalStorage(postData: any) {
+  this.localStorageService.set('posts', postData);
+}
+
 
 handleFileInput(event: any) {
   const files: FileList = event.target.files;
@@ -30,5 +38,11 @@ onSubmit() {
   formData.append('postBody', this.postBody);
   formData.append('postTags', this.postTags);
   formData.append('postPic', this.postPic as Blob);
+  
+  this.postTitle='';
+  this.postBody='';
+  this.postTags='';
+  this.postPic=null;
 }
+
 }
