901f2bd543784db64c57acceae2aed84ad83017f
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-10-20 19:24:00 +0200

    Prepared "new post "form functionality and "preview" of selected image

--- src/app/pages/create-post/create-post.component.ts
+++ src/app/pages/create-post/create-post.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent {
+postTitle: string = '';
+postBody: string = '';
+postTags: string = '';
+postPic: File | null = null; 
 
+handleFileInput(event: any) {
+  const files: FileList = event.target.files;
+  if (files.length > 0) {
+    this.postPic = files[0]; 
+  }
+}
+getImageUrl() {
+  if (this.postPic) {
+    return URL.createObjectURL(this.postPic);
+  }
+  return ''; 
+}
+
+onSubmit() {
+  const formData = new FormData();
+  formData.append('postTitle', this.postTitle);
+  formData.append('postBody', this.postBody);
+  formData.append('postTags', this.postTags);
+  formData.append('postPic', this.postPic as Blob);
+}
 }
