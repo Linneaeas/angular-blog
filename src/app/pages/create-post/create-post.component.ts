2147f430f5b62cb2af113fc897fc02030a5f8bd6
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-10-21 11:33:53 +0200

    Image is handled properly

--- src/app/pages/create-post/create-post.component.ts
+++ src/app/pages/create-post/create-post.component.ts
@@ -7,56 +7,44 @@ import { LocalStorageService } from 'src/app/local-storage.service';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent {
-postTitle: string = '';
-postBody: string = '';
-postTags: string = '';
-postPic: File | null = null; 
-imageUrl: string = ''; 
+  postTitle: string = '';
+  postBody: string = '';
+  postTags: string = '';
+  postPic: File | null = null;
+  imageUrl: string = '';
 
+  constructor(private localStorageService: LocalStorageService) {}
 
-constructor(private localStorageService: LocalStorageService) {}
+  handleFileInput(event: any) {
+    const files: FileList = event.target.files;
+    if (files.length > 0) {
+      this.postPic = files[0];
+      this.imageUrl = URL.createObjectURL(this.postPic); 
+    }
+  }
 
-savePostToLocalStorage(postData: any) {
-  this.localStorageService.set('posts', postData);
-}
+  onSubmit() {
+    if (!this.postPic) {
+      return;
+    }
 
+    const newPost = {
+      postTitle: this.postTitle,
+      postBody: this.postBody,
+      postTags: this.postTags,
+      postPic: this.imageUrl,
+    };
+    
+    let posts: any[] = this.localStorageService.get('posts') || [];
+    posts.push(newPost);
 
-handleFileInput(event: any) {
-  const files: FileList = event.target.files;
-  if (files.length > 0) {
-    this.postPic = files[0];
-    this.imageUrl = URL.createObjectURL(this.postPic); 
+    this.localStorageService.set('posts', posts);
+
+    this.postTitle = '';
+    this.postBody = '';
+    this.postTags = '';
+    this.postPic = null;
+    this.imageUrl = '';
   }
 }
-getImageUrl() {
-  return this.imageUrl; 
-}
 
-onSubmit() {
-  const imageUrl = this.postPic ? URL.createObjectURL(this.postPic) : '';
-
-  const newPost = {
-    postTitle: this.postTitle,
-    postBody: this.postBody,
-    postTags: this.postTags,
-    postPic: this.imageUrl,
-  };
-  let posts: any[] = this.localStorageService.get('posts') || [];
-  posts.push(newPost);
-
-  this.localStorageService.set('posts', posts);
-
-  const formData = new FormData();
-  formData.append('postTitle', this.postTitle);
-  formData.append('postBody', this.postBody);
-  formData.append('postTags', this.postTags);
-  formData.append('postPic', this.postPic as Blob);
-  
-  this.postTitle='';
-  this.postBody='';
-  this.postTags='';
-  this.postPic=null;
-  this.imageUrl = '';
-}
-
-}
